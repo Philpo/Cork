3a8f918bb3a47afb9ebcb0ef090981063d57d883
Chris Phillips <chris.ph@blueyonder.co.uk>
2016-02-12 13:24:27 +0000

    Added Scheduler class (first pass, sill needs work)

---
+++ src/Cork/Cork/Scheduler.cpp
@@ -0,0 +1,102 @@
+#include "Scheduler.h"
+
+Scheduler::Scheduler() : timeLastFrame(0.0), frameRate(0.0), function(nullptr), caption(L"") {}
+
+Scheduler::Scheduler(const Window& window, double frameRateTarget, const wstring caption) : 
+  timeLastFrame(0.0), frameRate(1000.0 / frameRateTarget), function(nullptr), caption(caption), window(window) {
+  startCounter();
+}
+
+WPARAM Scheduler::gameLoop() {
+  MSG msg = { 0 };
+
+  while (WM_QUIT != msg.message) {
+    if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+      switch (msg.message) {
+        // forward Windows key messages to the DirectXTK keyboard handler
+        case WM_ACTIVATEAPP:
+        case WM_KEYDOWN:
+        case WM_SYSKEYDOWN:
+        case WM_KEYUP:
+        case WM_SYSKEYUP:
+          // TODO maybe use dx toolkit keyboard class for input handling
+          //Keyboard::ProcessMessage(msg.message, msg.wParam, msg.lParam);
+          break;
+      }
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+    else {
+      calculateFrameRateStats();
+      double currentTime = getCounter();
+      double elapsedTime = currentTime - timeLastFrame;
+
+      //TODO figure out how to poll components every frame; is there a better way than the below?
+      for (auto component : pollEveryFrame) {
+        if (component->getType() == INPUT_COMPONENT) {
+          //component->receiveMessage(InputPollMessage());
+        }
+      }
+
+      if (function) {
+        function(elapsedTime);
+      }
+
+      double sleepTime = frameRate - (getCounter() - currentTime);
+      if (sleepTime > 0) {
+        Sleep(sleepTime);
+      }
+      timeLastFrame = currentTime;
+    }
+  }
+  return msg.wParam;
+}
+
+/*
+* Taken from https://stackoverflow.com/questions/1739259/how-to-use-queryperformancecounter
+*/
+HRESULT Scheduler::startCounter() {
+  LARGE_INTEGER li;
+  if (!QueryPerformanceFrequency(&li)) {
+    return -1;
+  }
+
+  freq = double(li.QuadPart) / 1000.0;
+
+  QueryPerformanceCounter(&li);
+  counterStart = li.QuadPart;
+
+  return S_OK;
+}
+
+/*
+* Taken from https://stackoverflow.com/questions/1739259/how-to-use-queryperformancecounter
+*/
+double Scheduler::getCounter() {
+  LARGE_INTEGER li;
+  QueryPerformanceCounter(&li);
+  return double(li.QuadPart - counterStart) / freq;
+}
+
+/*
+* Taken from Frank Luna: 3D Game Programming with DirectX 11
+*/
+void Scheduler::calculateFrameRateStats() {
+  static int frameCnt = 0;
+  static double timeElapsed = 0.0;
+
+  frameCnt++;
+
+  if (getCounter() - timeElapsed >= 1.0f) {
+    float fps = (float) frameCnt;
+    float mspf = 1000.0f / fps;
+
+    wostringstream outs;
+    outs.precision(6);
+    outs  << L"Cork    " << L"FPS: " << fps << L"    " << L"Frame Time: " << mspf << L" (ms)";
+    window.setWindowCaption(outs);
+
+    frameCnt = 0;
+    timeElapsed += 1000.0;
+  }
+}
---
+++ src/Cork/Cork/Scheduler.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "IComponent.h"
+#include "Window.h"
+#include <vector>
+
+using namespace std;
+
+typedef void (* gameLoopFunction)(double);
+
+class Scheduler {
+public:
+  Scheduler();
+  Scheduler(const Window& window, double frameRateTarget, const wstring caption);
+  ~Scheduler() {}
+
+  inline void registerPollComponent(IComponent& component) { pollEveryFrame.push_back(&component); }
+  inline void setGameLoopFunction(gameLoopFunction function) { this->function = function; }
+
+  WPARAM gameLoop();
+private:
+  double timeLastFrame, frameRate, freq;
+  __int64 counterStart; 
+  wstring caption;
+  Window window;
+  gameLoopFunction function;
+  vector<IComponent* const> pollEveryFrame;
+
+  HRESULT startCounter();
+  double getCounter();
+  void calculateFrameRateStats();
+};
