5b499b31e090fca52130f4dec884ce284150a7e6
Philpo <chris.ph@blueyonder.co.uk>
2016-04-24 17:22:39 +0100

    Functions added to allow GameObject components to be removed. Report updated

--- src/Cork/Cork/GameObject.h
+++ src/Cork/Cork/GameObject.h
@@ -23,9 +23,11 @@ public:
   void setUUId(int uuid) { this->uuid = uuid; }
 
   void addDataComponent(const string& type, IDataComponent* const component) { dataComponents.insert(pair<string, IDataComponent* const>(type, component)); }
+  void removeDataComponent(const string& type) { dataComponents.erase(type); }
   IDataComponent* const getDataComponent(const string& type) const { return dataComponents.find(type) != dataComponents.end() ? dataComponents.at(type) : nullptr; }
 
   void addComponent(const string& type, IComponent* const component);
+  void removeComponent(const string& type) { components.erase(type); }
   IComponent* const getMessageHandler(const string& type) const { return components.find(type) != components.end() ? components.at(type) : nullptr; }
 protected:
   int uuid;
